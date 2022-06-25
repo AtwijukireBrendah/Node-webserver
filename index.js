14f33b7676d42ea28996a974a1c102cef5f092d0
AtwijukireBrendah <kire5brenda@gmail.com>
2022-06-26 00:29:54 +0300

    node server serving about,contact and home files

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const http =require('http');
+const url =require('url');
+const fs =require('fs');
+const { isAbsolute } = require('path');
+
+// create server
+const server = http.createServer(
+function(request,response) {
+    //get the pathname
+
+    const parsedUrl = url.parse(request.url)
+    const filename =parsedUrl.pathname !=="/" ? `.${parsedUrl.pathname}.html` : './index.html';
+    // ./about.html
+
+fs.readFile(filename,(err,data)=>{
+    if(data){
+        response.write(data);
+        response.end();
+    }else{
+        fs.readFile('./404.html'),(err,data)=>{
+        response.write(data);
+        response.end();
+        }
+        response.write(data);
+        response.end();
+    }
+    
+})
+
+}  
+ )
+//Listen for requests
+server.listen(5000)
+
